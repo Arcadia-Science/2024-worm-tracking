1288e83a4b179373c9cbf54664507e7879a64e25
taylorreiter <taylorreiter@gmail.com>
2024-09-20 06:50:36 -0700

    rm network stuff from launch

--- scripts/tierpsy_linux_no_gui_background.sh
+++ scripts/tierpsy_linux_no_gui_background.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 local_folder_to_mount="$HOME/github/2024-worm-tracking"
-network_folder_to_mount=""
 
 # Check if Docker service is active, start or restart if necessary
 if ! systemctl is-active --quiet docker; then
@@ -20,19 +19,10 @@ fi
 # Create array of arguments for docker run.
 docker_arguments=(-d --rm)
 docker_arguments+=(--name my_tierpsy_container)
-docker_arguments+=(--sysctl net.ipv4.tcp_keepalive_intvl=30)
-docker_arguments+=(--sysctl net.ipv4.tcp_keepalive_probes=5)
-docker_arguments+=(--sysctl net.ipv4.tcp_keepalive_time=100)
-docker_arguments+=(--hostname tierpsydocker)
 
 # if local folder not empty, add to array
 if [[ ! -z "$local_folder_to_mount" ]]; then
     docker_arguments+=(-v "${local_folder_to_mount}:/DATA/local_drive")
-fi
-
-# if network folder is not empty, add to array
-if [[ ! -z "$network_folder_to_mount" ]]; then
-    docker_arguments+=(-v "${network_folder_to_mount}:/DATA/network_drive")
 fi
 
 # launch using the parameters in the array
